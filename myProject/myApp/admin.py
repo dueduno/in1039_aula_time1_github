3e578302489eedad5c1c67004811c895641acfa9
thiagodc <thiagodamicoc@gmail.com>
2025-05-11 21:22:27 -0300

    integração do banco

--- myProject/myApp/admin.py
+++ myProject/myApp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Cliente, Administrador, Estacionamento, Possui, Vaga, Contem, Reserva
+
+admin.site.register(Cliente)
+admin.site.register(Administrador)
+admin.site.register(Estacionamento)
+admin.site.register(Possui)
+admin.site.register(Vaga)
+admin.site.register(Contem)
+admin.site.register(Reserva)
 
 # Register your models here.
--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Cliente, Administrador, Estacionamento, Possui, Vaga, Contem, Reserva
 
 def home(request):
-    context={}
-    return render(request, "myApp/home.html",context)
+    cliente = Cliente.objects.all()
+    administrador = Administrador.objects.all()
+    estacionamento=Estacionamento.objects.all()
+    possui=Possui.objects.all()
+    vaga=Vaga.objects.all()
+    contem=Contem.objects.all()
+    reserva=Reserva.objects.all()
+    return render(request, "myApp/home.html",context={"cliente":cliente}) #Perguntar na aula se eu posso passar uma lista de dicionarios 
