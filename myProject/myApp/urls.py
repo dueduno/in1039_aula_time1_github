7b27ab0338a9d61042298a77068125e917bca87f
thiagodc <thiagodamicoc@gmail.com>
2025-05-13 23:11:24 -0300

    login html com funcionalidade

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -4,8 +4,10 @@ from django.contrib import admin
 
 urlpatterns = [
     path("users/", views.create_user, name="user"),
+    path("users/login/", views.login_user, name="login"),
+    path("users/logout/", views.logout_user, name="logout"),
     #path('users/',include('django.contrib.auth.urls')),
-    path("", views.home, name="home"),          
+    path("home/", views.home, name="home"),          
     path("entrada/", views.entrada, name="entrada"),  
     path("base/", views.base, name="base"),
 ]
--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login,logout
 from .models import Cliente, Administrador, Estacionamento, Possui, Vaga, Contem, Reserva
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def home(request):
     # cliente = Cliente.objects.filter(nome = "joao").all()
     cliente = Cliente.objects.all()
@@ -24,6 +27,7 @@ def home(request):
 
     return render(request, "home.html",context=context)
 
+@login_required
 def base(request):
     # cliente = Cliente.objects.filter(nome = "joao").all()
     cliente = Cliente.objects.all()
@@ -47,6 +51,7 @@ def base(request):
     return render(request, "base.html",context=context)
 
 
+@login_required
 def entrada(request):
     # cliente = Cliente.objects.filter(nome = "joao").all()
     cliente = Cliente.objects.all()
@@ -79,3 +84,25 @@ def create_user(request):
         user.save
         return redirect("home")
     return render(request,"register.html",context={"action":"Adicionar"})
+
+
+
+def login_user(request):
+    if request.method=="POST":
+        user=authenticate(
+            username=request.POST["username"],
+            password=request.POST["password"]
+            )
+        if user!=None:
+            login(request,user)
+        else:
+            return render(request,"login.html",context={"error_msg":"Usuário não existe"})
+
+        if request.user.is_authenticated:
+            return redirect("home")
+        return render(request,"login.html",context={"error_msg":"Usuário não pode ser autenticado"})
+    return render(request,"login.html")
+
+def logout_user(request):
+    logout(request)
+    return redirect("login")
