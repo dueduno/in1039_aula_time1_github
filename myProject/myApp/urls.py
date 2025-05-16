305d31cb92a6f0d27e60ac9292ecee403839a72e
thiagodc <thiagodamicoc@gmail.com>
2025-05-16 11:11:44 -0300

    path vazio agora é a home

--- myProject/myApp/urls.py
+++ myProject/myApp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("users/login/", views.login_user, name="login"),
     path("users/logout/", views.logout_user, name="logout"),
     #path('users/',include('django.contrib.auth.urls')),
-    path("home/", views.home, name="home"),          
+    path("", views.home, name="home"),          
     path("entrada/", views.entrada, name="entrada"),  
     path("base/", views.base, name="base"),
 ]
